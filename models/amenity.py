1f3cd1b0533f3dc61deaa48abe25c51a45b05b0d
Oumaima SLL <58479472+emaoumaima@users.noreply.github.com>
2024-01-18 01:04:28 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Amenity Module for HBNB project """
+""" The HBNB project Amenity Module """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
